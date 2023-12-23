1881b762e242526d3ef5576c0c9411366f41df15
Nicola Gallo <nicolagmt@hotmail.com>
2023-12-23 17:23:19 +0100

    Automated commit.

--- internal/agents/pdp/services/local_service_test.go
+++ internal/agents/pdp/services/local_service_test.go
@@ -5,27 +5,70 @@ package services
 
 import (
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/autenticami/autenticami-authz/pkg/iam/accessmanagement/permissions"
 	authzIntAgentConfigs "github.com/autenticami/autenticami-authz/internal/agents/configs"
 	authzIntPdpAgentConfigs "github.com/autenticami/autenticami-authz/internal/agents/pdp/configs"
+	"github.com/autenticami/autenticami-authz/pkg/iam/accessmanagement/permissions"
+	"github.com/autenticami/autenticami-authz/pkg/iam/accessmanagement/policies"
 )
 
-func TestPDPAgentConfig(t *testing.T) {
-	assert := assert.New(t)
-	os.Setenv(authzIntAgentConfigs.EnvKeyAutenticamiAgentAppData, "./testdata/local-service/autenticami1")
-	config, err := authzIntPdpAgentConfigs.NewPDPAgentConfig()
-	assert.Nil(err, "wrong result\nerr shold be nil and not % s", spew.Sdump(err))
-	service := NewPDPLocalService(config)
-	settings := []permissions.PermissionsEngineOption{
-		permissions.WithPermissionsEngineVirtualState(true),
-		permissions.WithPermissionsEngineVirtualStateViewCombined(true),
+func TestBuildPermissionsState(t *testing.T) {
+	type TestStruct struct {
+		Name                string
+		User                string
+		Combined			bool
+		LenOfForbid			int
+		LenOfPermit			int
 	}
-	permState, err := service.GetPermissionsState("", settings[:]...)
-	assert.Nil(err, "wrong result\nerr shold be nil and not %s", spew.Sdump(err))
-	assert.NotNil(permState, "wrong result\npermState shold be not nil")
+	tests := []TestStruct{
+		{
+			"EMPTY-NOT-COMBINED",
+			"",
+			false,
+			0,
+			0,
+		},
+		{
+			"USER-NOT-COMBINED",
+			"uur:581616507495:default:autenticami:iam:user/nicola.gallo@nitroagility.com",
+			false,
+			1,
+			9,
+		},
+		{
+			"USER-COMBINED",
+			"uur:581616507495:default:autenticami:iam:user/nicola.gallo@nitroagility.com",
+			true,
+			1,
+			3,
+		},
+	}
+	for _, test := range tests {
+		t.Run(strings.ToUpper(test.Name), func(t *testing.T) {
+			assert := assert.New(t)
+			os.Setenv(authzIntAgentConfigs.EnvKeyAutenticamiAgentAppData, "./testdata/local-service")
+			config, err := authzIntPdpAgentConfigs.NewPDPAgentConfig()
+			assert.Nil(err, "wrong result\nerr shold be nil and not % s", spew.Sdump(err))
+			service := NewPDPLocalService(config)
+			_ = service.Setup()
+			settings := []permissions.PermissionsEngineOption{
+				permissions.WithPermissionsEngineVirtualState(true),
+				permissions.WithPermissionsEngineVirtualStateViewCombined(test.Combined),
+			}
+			permState, err := service.GetPermissionsState(policies.UURString(test.User), settings[:]...)
+			assert.Nil(err, "wrong result\nerr shold be nil and not %s", spew.Sdump(err))
+			assert.NotNil(permState, "wrong result\npermState shold be not nil")
+
+			forbidden, _ := permState.GetACLForbiddenPermissions()
+			assert.Equal(test.LenOfForbid, len(forbidden), "wrong result\nforbidden shold be equale to 0")
+
+			permit, _ := permState.GetACLPermittedPermissions()
+			assert.Equal(test.LenOfPermit, len(permit), "wrong result\nforbidden shold be equale to 0")
+		})
+	}
 }
